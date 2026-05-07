43c4fd8c4e96ae233b62ae13d722cf6d31e527c9
vagheit-jpg <vagheit@gmail.com>
2026-05-07 20:23:49 +0900

    Update technicalEngine.js

--- src/engines/technicalEngine.js
+++ src/engines/technicalEngine.js
@@ -1,1 +1,19 @@
+import { ema } from "./mathEngine";
 
+export const calcMACD = (monthly) => {
+  const cl = monthly.map(d => d.price);
+
+  const e12 = ema(cl, 12);
+  const e26 = ema(cl, 26);
+
+  const macd = cl.map((_, i) => +(e12[i] - e26[i]));
+
+  const sig = ema(macd, 9);
+
+  return monthly.map((d, i) => ({
+    ...d,
+    macd: macd[i],
+    signal: sig[i],
+    hist: +(macd[i] - sig[i]),
+  }));
+};
