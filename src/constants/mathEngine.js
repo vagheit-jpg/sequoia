6fbb6252a4da75ab560ea253ca10dd01c368ea31
vagheit-jpg <vagheit@gmail.com>
2026-05-07 20:16:56 +0900

    Update mathEngine.js

--- src/constants/mathEngine.js
+++ src/constants/mathEngine.js
@@ -1,1 +1,12 @@
+export const ema = (arr, n) => {
+  const k = 2 / (n + 1);
+  let e = arr[0];
 
+  return arr.map((v, i) => {
+    if (i === 0) return e;
+
+    e = v * k + e * (1 - k);
+
+    return +e.toFixed(2);
+  });
+};
