212d2d94d211ee92394cdf7954ffa8783c2b29b8
vagheit-jpg <vagheit@gmail.com>
2026-05-16 22:35:17 +0900

    Add files via upload

--- src/components/common/JarvisComment.jsx
+++ src/components/common/JarvisComment.jsx
@@ -132,6 +132,7 @@ export default function JarvisComment({ C, tabType, ticker = null, name = null, 
     fontFamily: 'monospace',
     flexShrink: 0,
     lineHeight: 1,
+    paddingTop: 2,
   };
 
   const summaryStyle = {
